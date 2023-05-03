7fda3de28782953eaa9d92381dd6e41a86e7d2da
DarnUCoolNames <tntran@arizona.edu>
2023-05-03 16:56:50 -0700

    Adding Character Movement

--- NIGHTMARE Lesson/Assets/Scripts/PlayerController.cs
+++ NIGHTMARE Lesson/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float walkSpeed = 5000.0f;
+    public float rotateSpeed = 10000.0f;
+
+    public GameObject player;
+
+    Animator animController;
+    Rigidbody rb;
+
     void Start()
     {
-        
+        animController = player.GetComponent<Animator>();
+        rb      = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // if (Input.GetAxis("Vertical"))
+        // {
+        //     transform.Translate(0, 0, walkSpeed * Time.deltaTime);
+        //     animController.SetBool("Walk", true);
+        // }
+
+        // if (Input.GetAxis("Horizontal"))
+        // {
+        //     transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        //     animController.SetBool("Walk", true);
+        // }
+        // else
+        // {
+        //     animController.SetBool("Walk", false);
+        // }
+        Vector3 input = new Vector3(0, Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (input.magnitude > 0.001)
+        {
+            rb.AddRelativeTorque(new Vector3(0, input.y * rotateSpeed * Time.deltaTime, 0));
+            
+            rb.AddRelativeForce(new Vector3(0, 0, input.z * walkSpeed * Time.deltaTime));
+
+            animController.SetBool("Walk", true);
+        }
+        else
+        {
+            animController.SetBool("Walk", false);
+        }
+
     }
 }
