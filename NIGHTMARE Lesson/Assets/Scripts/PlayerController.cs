23c77b1822cb3ded81debeb5de944b3a05192392
DarnUCoolNames <tntran@arizona.edu>
2023-05-06 22:05:16 -0700

    Added Hallway Parkour

--- NIGHTMARE Lesson/Assets/Scripts/PlayerController.cs
+++ NIGHTMARE Lesson/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 70000.0f;
     public float rotateSpeed = 10000.0f;
     public float jumpHeight;
-    public bool isGrounded = true;
+    bool isGrounded = true;
+    bool isAlive = true;
+    //int health = 10;
 
     public GameObject player;
 
@@ -24,7 +26,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 input = new Vector3(0, Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        if (input.magnitude > 0.001)
+        if (input.magnitude > 0.001 && isAlive)
         {
             rb.AddRelativeTorque(new Vector3(0, input.y * rotateSpeed * Time.deltaTime, 0));
             
@@ -38,21 +40,20 @@ public class PlayerController : MonoBehaviour
             animController.SetBool("Walk", false);
             animController.SetBool("isGrounded", true);
         }
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded && isAlive)
         {
             rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
             isGrounded = false;
             animController.SetBool("Jumped", true);
             animController.SetBool("isGrounded", false);
         }
-
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Floor")
         {
             isGrounded = true;
-            
         }
     }
 }
