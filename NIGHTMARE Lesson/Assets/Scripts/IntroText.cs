b140ce5b025b2e38098ea110719e1e72da29a7f4
Hannah <hannah.klecan@gmail.com>
2023-05-10 19:58:14 -0700

    intro text overlay

---
+++ NIGHTMARE Lesson/Assets/Scripts/IntroText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroText : MonoBehaviour
+{
+
+    public GameObject Object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object.SetActive(false);
+    }
+
+    void OnTriggerEnter(){
+
+        Object.SetActive(true);
+
+    }
+
+    void OnTriggerExit(){
+
+        Object.SetActive(false);
+
+    }
+}
